fbd726f62c99c7c07878e6176e0b135ed2fea945
mikeyp957 <peraram2@gmail.com>
2022-03-09 15:50:51 -0800

    add http module

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { HttpModule } from "@angular/http";
 
+// containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+
+// components
 import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
 import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
+
+// service
+import { PassengerDashboardService } from "./passenger-dashboard.service";
+
+
 
 @NgModule({
     declarations: [
@@ -12,11 +21,15 @@ import { PassengerDetailComponent } from "./components/passenger-detail/passenge
         PassengerDetailComponent
     ],
     imports:[
-        CommonModule
+        CommonModule,
+        HttpModule
     ],
     exports: [
         PassengerDashboardComponent
     ],
+    providers: [
+        PassengerDashboardService
+    ]
 })
 
 export class PassengerDashboardModule{}
