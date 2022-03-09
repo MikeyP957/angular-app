02f5eeb24713be6e6e7075331a145c14e0b442dd
mikeyp957 <peraram2@gmail.com>
2022-03-08 23:27:41 -0800

    refactor

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
@@ -6,57 +6,65 @@ import { Passenger } from '../../models/passenger.interface';
     styleUrls:['passenger-dashboard.component.scss'],
     template: `
         <div>
-        <h3>Airline Passengers</h3>
-        <ul>
-            <li *ngFor= "let passenger of passengers; let i = index">
-            <span 
-            class="status"
-            [class.checked-in]="passenger.checkedIn"
-            >
-            </span>
-            {{ i + 1 }}: {{ passenger.fullname }}
-            </li>
-        </ul>
+        <passenger-count
+            [items]="passengers">
+        </passenger-count>
+        <passenger-detail
+            *ngFor="let passenger of passengers"
+            [detail]="passenger"
+            (edit)="handleEdit($event)"
+            (remove)="handleRemove($event)">
+        </passenger-detail>
         </div>
     `
 })
 
-export class PassengerDashboardComponent{
-    passengers: Passenger[] =[
-        {
-          id:1,
-          fullname: 'Langston Highland-Perara',
-          checkedIn: true,
-          checkInDate: null,
-          children: null
-        },
-        {
-          id:2,
-          fullname: 'Theodore Highland-Perara',
-          checkedIn: false,
-          checkInDate: null,
-          children: null
-          },
-        {
-          id:3,
-          fullname: 'Frank Lunde',
-          checkedIn: false,
-          checkInDate: null,
-          children: null
-        },
-        {
-          id:4,
-          fullname: 'Edgar Lunde',
-          checkedIn: true,
-          checkInDate: null,
-          children: null
-        },
-        {
-          id:5,
-          fullname: 'Henry Lunde',
-          checkedIn: true,
-          checkInDate: null,
-          children: null
-        },
-      ]
+export class PassengerDashboardComponent implements OnInit {
+    passengers: Passenger[];
+    constructor() {}
+    ngOnInit() {
+        this.passengers =[
+            {
+              id:1,
+              fullname: 'Langston Highland-Perara',
+              checkedIn: true,
+              checkInDate: null,
+              children: null
+            },
+            {
+              id:2,
+              fullname: 'Theodore Highland-Perara',
+              checkedIn: false,
+              checkInDate: null,
+              children: null
+              },
+            {
+              id:3,
+              fullname: 'Frank Lunde',
+              checkedIn: false,
+              checkInDate: null,
+              children: null
+            },
+            {
+              id:4,
+              fullname: 'Edgar Lunde',
+              checkedIn: true,
+              checkInDate: null,
+              children: null
+            },
+            {
+              id:5,
+              fullname: 'Henry Lunde',
+              checkedIn: true,
+              checkInDate: null,
+              children: null
+            },
+        ]
+    }
+    handleEdit(event){
+        console.log(event)
+    }
+    handleRemove(event) {
+        console.log(event)
+    }
 }
