3028b5cb613bc313fc0e05426538bd692c4481e1
mikeyp957 <peraram2@gmail.com>
2022-03-10 10:36:50 -0800

    add passenger-viewer to dashboard and app

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -4,6 +4,7 @@ import { HttpModule } from "@angular/http";
 
 // containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
+import { PassengerViewerComponent } from "./containers/passenger-viewer/passenger-viewer.component";
 
 // components
 import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
@@ -17,6 +18,7 @@ import { PassengerDashboardService } from "./passenger-dashboard.service";
 @NgModule({
     declarations: [
         PassengerDashboardComponent,
+        PassengerViewerComponent,
         PassengerCountComponent,
         PassengerDetailComponent
     ],
@@ -25,7 +27,7 @@ import { PassengerDashboardService } from "./passenger-dashboard.service";
         HttpModule
     ],
     exports: [
-        PassengerDashboardComponent
+        PassengerViewerComponent
     ],
     providers: [
         PassengerDashboardService
